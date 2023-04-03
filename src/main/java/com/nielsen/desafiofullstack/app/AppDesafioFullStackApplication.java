e9e69a21e414ef670960261cfd61626b80b3aff7
Marcio Nielsen Baptista <marcio.nielsen@gmail.com>
2023-04-03 16:49:58 -0300

    Código inicial do projeto.

---
+++ src/main/java/com/nielsen/desafiofullstack/app/AppDesafioFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.nielsen.desafiofullstack.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDesafioFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDesafioFullStackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nielsen/desafiofullstack/app/AppDesafioFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nielsen.desafiofullstack.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDesafioFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
